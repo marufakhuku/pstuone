e9a36695fde732519428e386ba48611c7faa0137
unknown <marufakhuku02@gmail.com>
2024-10-16 10:47:47 -0700

    first commit

---
+++ five.js
@@ -0,0 +1,10 @@
+function checkEvenOrOdd(number){
+    if (number%2===0){
+        console.log(number+ "This is a even numbers.");
+
+    }
+    else{
+        console.log(number+"This is a odd number.");
+        
+    }
+}
